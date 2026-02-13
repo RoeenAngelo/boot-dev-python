3cae2d5f05e2bbce31b1505c4a33cb05485c50b2
roeenangelo <roeenangelo@gmail.com>
2026-02-14 00:21:57 +0800

    Add exercise

---
+++ Exercises/using_a_stack.py
@@ -0,0 +1,44 @@
+# Using a Stack
+# LockedIn supports a basic scripting language. It allows technically savvy HR managers to write scripts that can automate repetitive tasks on the platform. The language makes use of parentheses to group operations together, and we need to be able to check if the parentheses in a script are balanced.
+
+# Balanced Parentheses
+# Parentheses are balanced when each parenthesis has a corresponding parenthesis, and the pairs of parentheses are properly nested. For example:
+
+# ()
+# ()()
+# ((()))
+# (()(()))
+# Unbalanced Parentheses
+# (
+# ())
+# (()()
+# (()))
+# Assignment
+# Complete the is_balanced function.
+
+# It takes a string as input and returns True if the parentheses in the string are balanced, and False otherwise. Use an instance of the provided Stack class in stack.py to keep track of the parentheses.
+
+# If the parentheses are balanced, then the stack should be empty.
+
+# You only need to consider the characters ( and ) for this challenge.
+
+
+
+from stack import Stack
+
+
+def is_balanced(input_str):
+    stack = Stack()
+
+    for ch in input_str:
+        if ch == "(":
+            stack.push(ch)
+        elif ch == ")":
+            if stack.pop() is None:
+                return False
+    return stack.peek() is None
+
+
+
+
+
